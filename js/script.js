de6794690e6f3d7dcbab6486a94b0d5c37b828ce
Santaclaus <santa@northpole.com>
2022-03-21 10:17:00 +0300

    add constructor and prototype

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 $(document).ready(function () {
-    $(".icons").click(function () {
-      $(this).find(".preference-image").toggle();
-      $(this).find(".hidden-p").toggle();
-    })
+  let pizzaSize = 0;
+  let crust = 0;
+  let topping = 0;
+  let orderNum = 0;
+
+  function Pizza(size, topping, crust) {
+    this.size = size;
+    this.topping = topping;
+    this.crust = crust;
+  }
+
+  Pizza.prototype.addCost = function(){
+    return this.size + this.topping + this.crust;
+  }
+
+  $("#add-to-cart").click(function(){
+    pizzaSize = parseInt($("#pizza-size").val());
+  topping = parseInt($("#topping").val());
+  crust = parseInt($("#crust").val());
+
+    orderNum += 1;
+
+  let newPizza = new Pizza(pizzaSize, topping, crust);
+
+    console.log(newPizza);
+    console.log(newPizza.addCost());
+    $("tbody").append(`<tr><th scope='row'>${orderNum}</th><td>${newPizza.size}</td><td>${newPizza.crust}</td><td>${newPizza.topping}</td></tr>`)
+  })
+
+})
